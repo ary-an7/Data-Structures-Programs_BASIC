8b4948bae231d6de47d95fb3743a5de83051e242
Aryan Pratap <aryanpratap98765@gmail.com>
2021-04-26 00:23:50 +0530

    Create linear-queue.c

---
+++ Queue-implementation/linear-queue.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#define QUE_SIZE 5
+void insertQ();
+int deleteQ();
+void display();
+int front=0,rear=-1;
+int item,q[QUE_SIZE];
+void main()
+{
+	int choice;
+	
+	for(;;)
+	{
+		printf("1:Insert 2:Delete 3:Display 4:Exit\n");
+		printf("Enter your choice\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:printf("Enter the item to be inserted\n");
+			scanf("%d",&item);
+			insertQ();
+			break;
+			case 2:item=deleteQ();
+			if(front==-1)
+			printf("Queue is empty\n");
+			else
+			printf("The deleted item is %d\n",item);
+			break;
+			case 3:display();
+			break;
+			default: exit(0);
+		}
+	}
+}
+void insertQ()
+{
+	if(rear==QUE_SIZE-1)
+	{
+		printf("Queue overflow\n");
+		return;
+	}
+	rear=rear+1;
+	q[rear]=item;
+}
+int deleteQ()
+{
+	int item;
+	if(front>rear)
+	{
+		printf("Queue overflow\n");
+		return -1;
+	}
+	else
+	front=front+1;
+	return q[front];
+}
+void display()
+{
+	int i;
+	if(front>rear)
+	{
+		printf("Queue is empty\n");
+		return;
+	}
+	printf("The content of the queue are\n");
+	for(i=front;i<=rear;i++)
+	{
+		printf("%d\n",q[i]);
+
+	}
+}
